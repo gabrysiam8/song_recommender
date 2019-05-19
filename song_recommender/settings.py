468c8c61e3b8f48721afd328fef24e3529961a75
pappryk <patryknng@interia.pl>
2019-05-20 00:26:21 +0200

    Add heroku db connection

--- song_recommender/settings.py
+++ song_recommender/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = '*7t$j!8gn=+y_lkn-8hr@0u3f*e_)k7s2*+!kgkl-!-55+haq-'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-'mood-2-song.herokuapp.com'
+'mood-2-song.herokuapp.com',
+'ec2-54-221-198-156.compute-1.amazonaws.com'
 ]
 
 STATICFILES_DIRS = [
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'song_recommender.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'local': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'song_recommender',
         'USER': 'postgres',
@@ -91,13 +92,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-    'heroku': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'song_recommender',
-        'USER': 'postgres',
-        'PASSWORD': 'passw0rd',
-        'HOST': 'localhost',
-        'PORT': '',
+        'USER': 'oorvyhetzmebbg',
+        'PASSWORD': '5e89d90da075e8dfff5ac3fe227eefcc7e8f21feaa90fb05f0d8cb283ab36b0a',
+        'HOST': 'ec2-54-221-198-156.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
