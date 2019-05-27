3c7557f9993eec25a28f226477a20f06f784cc50
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-05-27 20:25:27 +0200

    Give random 50 mood names to main view

--- recommender/views.py
+++ recommender/views.py
@@ -3,12 +3,5 @@ from recommender.models import Mood
 
 
 def index(request):
-    sql = """
-        SELECT id, name, categories FROM 
-        ( SELECT *, ROW_NUMBER() 
-         OVER (PARTITION BY categories) AS RowNo 
-         FROM recommender_mood) AS rank 
-         WHERE rank.RowNo<=5
-    """
-    mood_names = [m.name for m in Mood.objects.raw(sql)]
+    mood_names = [m.name for m in Mood.objects.order_by('?')[:50]]
     return render(request, 'index.html', {'moods': mood_names})
