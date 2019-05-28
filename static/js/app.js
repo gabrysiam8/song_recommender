296ec0a2bde95e871243bcb2a2a21343adf1a968
pomarka <mariapomaranska@gmail.com>
2019-05-28 15:28:03 +0200

    Add changing color of selected moods

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+//modal box for "about"
 var modal = document.querySelector(".modal");
 var trigger = document.querySelector("#nav1");
 var closeButton = document.querySelector(".close-button");
@@ -16,17 +17,25 @@ trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-function changeStyleOfSelectedMoods() {
-    let li = document.querySelectorAll('.listOfMoods li');
+//changing color of chosen moods to white
+$("#list li").click(function() {
+    if ($(this).css("color") !== "white") {
+        ($(this).css("color", "white"));
+        ($(this).css("border", "solid 2px white"));
+        ($(this).css(":active"));
+    }
+});
 
-}
-
+//adding elements to the list of words with search bar and changing
+//their color to white
 function addChosenWords() {
     let word = document.querySelector('.moodsInput').value;
-    console.log(word);
 
     var el = document.getElementById("list");
     var node = document.createElement("li");
     node.innerText = word;
+    
     el.appendChild(node);
+    node.style.color = "white";
+    node.style.border = "solid 2px white";
 }
