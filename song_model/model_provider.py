3cc711d5ba832976585622c0ec21f3d7e1ae6827
pappryk <patryknng@interia.pl>
2019-06-14 20:04:30 +0200

    Uncomment generating model

--- song_model/model_provider.py
+++ song_model/model_provider.py
@@ -2,24 +2,24 @@ import gensim
 import logging
 import csv
 
-#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 songs = []
 
 # read csv
-#with open('songdata.csv') as csvfile:
-#    readCSV = csv.reader(csvfile, delimiter=',')
-#    for i, row in enumerate(readCSV):
-#        preprocessed = gensim.utils.simple_preprocess(str(row))
-#        songs.append(preprocessed)
+with open('songdata.csv') as csvfile:
+   readCSV = csv.reader(csvfile, delimiter=',')
+   for i, row in enumerate(readCSV):
+       preprocessed = gensim.utils.simple_preprocess(str(row))
+       songs.append(preprocessed)
 
 # train model
-#model = gensim.models.Word2Vec (songs, size=400, window=4, min_count=2, workers=10)
-#model.train(songs,total_examples=len(songs),epochs=10)
+model = gensim.models.Word2Vec (songs, size=400, window=4, min_count=2, workers=10)
+model.train(songs,total_examples=len(songs),epochs=10)
 
 # load model
-model = gensim.models.Word2Vec.load("songs_size400_window4_mincount2.model")
+# model = gensim.models.Word2Vec.load("songs_size400_window4_mincount2.model")
 
 # save model
-# model.save("songs_size400_window4_mincount2.model")
+model.save("songs_size400_window4_mincount2.model")
 
 print(model.wv.most_similar(positive=["depressed", "bad"]))
