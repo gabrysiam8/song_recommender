4cb23ea33640878513ee10228ed3365f1944d5a8
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-06-18 22:15:10 +0200

    Fix paths

--- recommender/models.py
+++ recommender/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-class Song(models.Model):
-    artist = models.TextField()
-    title = models.TextField()
-    link = models.TextField()
-    text = models.TextField()
-
-
 class Mood(models.Model):
     name = models.TextField()
     categories = ArrayField(models.TextField())
--- recommender/views.py
+++ recommender/views.py
@@ -5,8 +5,12 @@ from django.http import HttpResponse
 from .suggestions import find_song_suggestions
 from glob import glob
 import os
+import gensim
 
-all = []
+all_moods = []
+model = gensim.models.Word2Vec.load("song_model/songs_size400_window4_mincount2.model")
+word_vector = model.wv
+
 
 def index(request):
     for filepath in glob('data/keywords/*.txt'):
@@ -14,12 +18,13 @@ def index(request):
         with open(filepath, 'r') as file:
             moods = [mood.strip() for mood in file.read().split(',')]
             for mood_name in moods:
-                mood = Mood()
-                mood.name = mood_name
-                mood.categories = categories
-                all.append(mood)
+                if mood_name in word_vector.vocab:
+                    mood = Mood()
+                    mood.name = mood_name
+                    mood.categories = categories
+                    all_moods.append(mood)
     # mood_names = [m.name for m in Mood.objects.order_by('?')[:50]]
-    mood_names = [m.name for m in all[:50]]
+    mood_names = [m.name for m in all_moods[:50]]
     return render(request, 'index.html', {'moods': mood_names})
 
 
@@ -27,14 +32,15 @@ def get_moods(request):
     if request.is_ajax():
         q = request.GET.get('term', '')
         # moods = Mood.objects.filter(name__startswith=q)[:20]
-        moods = list(filter(lambda x: x.name.startswith(q), all))
+        moods = list(filter(lambda x: x.name.startswith(q), all_moods))
         results = []
         for mood in moods:
-            mood_json = {}
-            mood_json['id'] = mood.id
-            mood_json['label'] = mood.name
-            mood_json['value'] = mood.name
-            results.append(mood_json)
+            if mood.name in word_vector.vocab:
+                mood_json = {}
+                mood_json['id'] = mood.id
+                mood_json['label'] = mood.name
+                mood_json['value'] = mood.name
+                results.append(mood_json)
         data = json.dumps(results)
     else:
         data = 'fail'
