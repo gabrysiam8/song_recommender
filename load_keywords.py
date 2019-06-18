32fedb35c87f9b98f2ef2da5ff8a2438fc8113f4
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-06-18 19:25:29 +0200

    Temporary fix

--- load_keywords.py
+++ load_keywords.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import gensim
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'song_recommender.settings')
 
@@ -10,18 +11,20 @@ from recommender.models import Mood
 
 
 def save_moods_from_file(filepath):
+    model = gensim.models.Word2Vec.load("song_model/songs_size400_window4_mincount2.model")
+    word_vectors = model.wv
     categories = os.path.basename(filepath).split('.')[0].split('_')
     with open(filepath, 'r') as file:
         moods = [mood.strip() for mood in file.read().split(',')]
         for mood_name in moods:
-            mood = Mood()
-            mood.name = mood_name
-            mood.categories = categories
-            mood.save()
+            if mood_name in word_vectors.vocab:
+                mood = Mood()
+                mood.name = mood_name
+                mood.categories = categories
+                mood.save()
 
 
 if __name__ == "__main__":
-
     for filepath in glob('data/keywords/*.txt'):
         print('Reading from file ' + filepath)
         save_moods_from_file(filepath)
--- recommender/views.py
+++ recommender/views.py
@@ -3,17 +3,31 @@ from recommender.models import Mood
 import json
 from django.http import HttpResponse
 from .suggestions import find_song_suggestions
+from glob import glob
+import os
 
+all = []
 
 def index(request):
-    mood_names = [m.name for m in Mood.objects.order_by('?')[:50]]
+    for filepath in glob('data/keywords/*.txt'):
+        categories = os.path.basename(filepath).split('.')[0].split('_')
+        with open(filepath, 'r') as file:
+            moods = [mood.strip() for mood in file.read().split(',')]
+            for mood_name in moods:
+                mood = Mood()
+                mood.name = mood_name
+                mood.categories = categories
+                all.append(mood)
+    # mood_names = [m.name for m in Mood.objects.order_by('?')[:50]]
+    mood_names = [m.name for m in all[:50]]
     return render(request, 'index.html', {'moods': mood_names})
 
 
 def get_moods(request):
     if request.is_ajax():
         q = request.GET.get('term', '')
-        moods = Mood.objects.filter(name__startswith=q)[:20]
+        # moods = Mood.objects.filter(name__startswith=q)[:20]
+        moods = list(filter(lambda x: x.name.startswith(q), all))
         results = []
         for mood in moods:
             mood_json = {}
